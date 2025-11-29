f14e9f2879b91f245d9d8dd7d5e1923563d75a7c
Atharv9299 <atharv.rohal1113@gmail.com>
2025-11-29 19:32:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+input = (input("Enter yes or no:"))
+def shutdown():
+    if input == ("yes"):
+        print ("Shutting down")
+    elif input == ("no"):
+        print ("Abort shut down")
+    else:
+        print("Sorry")
+shutdown()    
+
+
+    
+    
